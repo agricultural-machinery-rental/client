1952f0d108ea96b515686e71dc32b66886d83b9b
djpont <alexandervotyakov@gmail.com>
2023-07-25 01:58:52 +0400

    feat: reset for global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,13 +7,22 @@ export const GlobalStyle = createGlobalStyle`
   * {
     margin:0;
     padding: 0;
+    vertical-align: baseline;
+    font-size: 1rem;
+    line-height: 1.15;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
   }
   
   body {
     font-family: Open-Sans, Helvetica, Sans-Serif, serif;
     font-size: 24px;
-    line-height: 1;
     background: #fff;
+    cursor: default;
   }
   
   #root,
@@ -25,7 +34,7 @@ export const GlobalStyle = createGlobalStyle`
     overflow: auto;
   }
 
-  ol, ul {
+  ol, ul, menu {
     list-style: none;
   }
 
@@ -34,6 +43,75 @@ export const GlobalStyle = createGlobalStyle`
     border-spacing: 0;
   }
 
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+
+  img, svg {
+    max-inline-size: 100%;
+    max-block-size: 100%;
+  }
+  
+  input, textarea {
+      -webkit-user-select: auto;
+  }
+
+  button, input, select, textarea {
+    font-family: inherit;
+  }
+
+  button, input {
+    overflow: visible;
+  }
+
+  button, select {
+    text-transform: none;
+  }
+
+  button,
+  [type="button"],
+  [type="reset"],
+  [type="submit"] {
+    -webkit-appearance: button;
+  }
+
+  button::-moz-focus-inner,
+  [type="button"]::-moz-focus-inner,
+  [type="reset"]::-moz-focus-inner,
+  [type="submit"]::-moz-focus-inner {
+    border-style: none;
+    padding: 0;
+  }
+
+  textarea {
+    white-space: revert;
+  }
+
+  meter {
+    -webkit-appearance: revert;
+    appearance: revert;
+  }
+
+  :where(pre) {
+    all: revert;
+  }
+
+  ::placeholder {
+    color: unset;
+  }
+
+  ::marker {
+    content: initial;
+  }
+
+  :where([contenteditable]:not([contenteditable="false"])) {
+    -moz-user-modify: read-write;
+    -webkit-user-modify: read-write;
+    overflow-wrap: break-word;
+    -webkit-line-break: after-white-space;
+    -webkit-user-select: auto;
+  }
 `;
 
 export const StFlex = styled.div<TFlexProps>(props => {
