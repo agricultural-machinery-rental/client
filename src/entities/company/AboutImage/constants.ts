a0763348449562fc79043254fc6b19355efed76d
djpont <alexandervotyakov@gmail.com>
2023-08-04 23:15:56 +0400

    feat: добавил картинку

--- src/entities/company/AboutImage/constants.ts
+++ src/entities/company/AboutImage/constants.ts
@@ -2,11 +2,6 @@ import { TAboutImage } from './typing';
 
 import AboutImg from '@/assets/images/250x180.png';
 
-export const aboutImageSize = {
-  width: 250,
-  height: 180,
-};
-
 export const aboutImageData: TAboutImage[] = [
   {
     image: AboutImg,
--- src/entities/company/AboutImage/index.ts
+++ src/entities/company/AboutImage/index.ts
@@ -1,5 +1,5 @@
 'use client';
 
 export { AboutImage } from './AboutImage';
-export { aboutImageData } from './aboutImageData';
+export { aboutImageData } from './constants';
 export { type TAboutImage } from './typing';
