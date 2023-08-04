283937af99d3aa7facfdf0e7381d70e85ceecfce
Alexander Votyakov <alexandervotyakov@gmail.com>
2023-08-05 00:26:44 +0400

    #12911378 about company page (#35)
    Co-authored-by: advatunes <dj-9-1-1@yandex.ru>

--- src/entities/company/AboutImage/AboutImage.tsx
+++ src/entities/company/AboutImage/AboutImage.tsx
@@ -1,17 +1,11 @@
 import React, { FC } from 'react';
-import { aboutImageSize } from './aboutImageData';
 import { StAboutImageWrap, StAboutImageItem } from './styled';
 import type { TAboutImage } from './typing';
 
 export const AboutImage: FC<TAboutImage> = ({ image, alt }) => {
   return (
     <StAboutImageWrap>
-      <StAboutImageItem
-        src={image}
-        width={aboutImageSize.width}
-        height={aboutImageSize.height}
-        alt={alt}
-      />
+      <StAboutImageItem src={image} alt={alt} />
     </StAboutImageWrap>
   );
 };
