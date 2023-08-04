a7d5b0b53062b344c4ec60dc6a2be49331134c93
Elena Nikiforova <vizualnoe@gmail.com>
2023-08-04 14:01:30 +0200

    fix: remove some props

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -4,16 +4,6 @@ import React, { FC } from 'react';
 import type { TButton } from '@/shared/Button/typing';
 import { StButton } from './styled';
 
-export const Button: FC<TButton> = ({ label, disabled = false, onClick, ...props }) => {
-  const handleClick = () => {
-    if (onClick && !disabled) {
-      onClick();
-    }
-  };
-
-  return (
-    <StButton onClick={handleClick} disabled={disabled} {...props}>
-      {label}
-    </StButton>
-  );
+export const Button: FC<TButton> = ({ label, ...props }) => {
+  return <StButton {...props}>{label}</StButton>;
 };
