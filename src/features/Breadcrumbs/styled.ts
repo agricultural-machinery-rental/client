19f578ae800822aff962692c125c1bc922c04b92
advatunes <dj-9-1-1@yandex.ru>
2023-08-11 21:42:32 +0300

    style: fix some styles

--- src/features/Breadcrumbs/styled.ts
+++ src/features/Breadcrumbs/styled.ts
@@ -1,12 +1,14 @@
 import { styled } from 'styled-components';
 
 import { StNextLink, StContainer } from '@/shared/styles/global';
+import { theme } from '@/shared/styles/theme';
 
 export const StBreadcrumbsItemWrap = styled(StContainer)`
   display: flex;
   font-size: 0.66rem;
   margin: 32px auto 0;
   align-items: center;
+  color: ${theme.colors.bgSecondary};
 `;
 export const StBreadcrumbsItem = styled.p`
   margin-right: 16px;
