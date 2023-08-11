63754a45d14b2113e959340a9f708decfd742b2a
advatunes <dj-9-1-1@yandex.ru>
2023-08-11 23:12:29 +0300

    feat: add rules-modal component

--- src/shared/modal/modalWindows.tsx
+++ src/shared/modal/modalWindows.tsx
@@ -1,3 +1,4 @@
+import { Rules } from '@/widgets/company/Rules';
 import { AuthForm } from '@/widgets/user/Auth';
 import { PasswordReset } from '@/widgets/user/PasswordReset';
 import { SignupForm } from '@/widgets/user/Signup';
@@ -6,10 +7,12 @@ export enum modalWindows {
   signin = 'signin',
   passwordReset = 'passwordReset',
   signup = 'signup',
+  rules = 'rules',
 }
 
 export const modalWindowContent = {
   [modalWindows.signin]: <AuthForm />,
   [modalWindows.passwordReset]: <PasswordReset />,
   [modalWindows.signup]: <SignupForm />,
+  [modalWindows.rules]: <Rules />,
 } as const;
