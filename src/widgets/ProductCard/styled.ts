e726cf4e63d0d49e5d8886caeed78792eb351ec1
Kirill Lyashenko <tltelf@yandex.ru>
2023-08-12 16:03:19 +0400

    refactor

--- src/widgets/ProductCard/styled.ts
+++ src/widgets/ProductCard/styled.ts
@@ -9,49 +9,65 @@ export const StSection = styled.section`
 
 export const StProductHeader = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 63px;
+  flex-direction: column;
+  margin-bottom: 26px;
 `;
 
 export const StProductTitle = styled(StHeading3)`
   margin: 0;
+  align-self: flex-start;
 `;
 
 export const StStarButton = styled.button`
   background-color: transparent;
   border: none;
   cursor: pointer;
+  align-self: flex-end;
+  margin-right: 20px;
 `;
 
 export const StProductMain = styled.div`
   display: grid;
-  grid-template-columns: 725px auto;
+  grid-template-columns: 536px auto;
   justify-content: space-between;
-  margin-bottom: 45px;
+  margin-bottom: 77px;
+  gap: 0 42px;
 `;
 
 export const StProductText = styled.p`
-  margin-bottom: 30px;
+  margin-bottom: 40px;
 `;
 
 export const StProductList = styled.ul`
   padding-left: 35px;
-  margin-bottom: 90px;
+  margin-bottom: 55px;
   list-style: disc;
+`;
+
+export const StProductItemInfo = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StProductFooter = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const StProductFooterInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: right;
+  margin-right: 100px;
+
+  & + div {
+    margin-right: 50px;
+  }
 `;
 
 export const StProductPrice = styled.span`
   font-weight: 700;
-  line-height: 1.333em;
-`;
-
-// TODO Стили и кнопка добавлены временно, пока не готова общая
-export const StBookingButton = styled.button`
-  width: 348px;
-  height: 80px;
-  font-weight: 600;
-  border: none;
-  border-radius: 8px;
-  background: #d9d9d9;
+  font-size: 1.333em;
+  line-height: 1.666em;
 `;
--- src/widgets/ProductCard/ui.tsx
+++ src/widgets/ProductCard/ui.tsx
@@ -1,17 +1,20 @@
+import { ButtonBooking } from '@/features/Button/ButtonBooking';
 import { Swiper } from '@/features/Swiper';
 
 import StarSVG from './assets/icons/star.svg';
 import { dataTractors } from './constants';
 import {
-  StBookingButton,
   StProductHeader,
   StProductList,
   StProductMain,
-  StProductPrice,
+  StProductItemInfo,
   StProductText,
   StProductTitle,
   StSection,
   StStarButton,
+  StProductFooter,
+  StProductFooterInfo,
+  StProductPrice,
 } from './styled';
 
 export const ProductCard = () => {
@@ -35,12 +38,42 @@ export const ProductCard = () => {
               <li>Возможность почасовой оплаты</li>
               <li>Подходит для ...</li>
             </StProductList>
-            <StProductPrice>13500 ₽/смена 8 часов</StProductPrice>
+            <ul>
+              <StProductItemInfo>
+                <span>Общая масса</span>
+                <span>7800 кг</span>
+              </StProductItemInfo>
+              <StProductItemInfo>
+                <span>Объём ковша</span>
+                <span>0,3 м³</span>
+              </StProductItemInfo>
+              <StProductItemInfo>
+                <span>Глубина копания</span>
+                <span>4150 мм</span>
+              </StProductItemInfo>
+              <StProductItemInfo>
+                <span>Ширина гусеницы</span>
+                <span>450 мм</span>
+              </StProductItemInfo>
+              <StProductItemInfo>
+                <span>Габариты</span>
+                <span>6080×2260×2640 мм</span>
+              </StProductItemInfo>
+            </ul>
           </div>
           <Swiper dataArray={dataTractors} />
         </StProductMain>
-        {/* TODO Кнопка добавлена на время, будет заменена на общую кнопку из entities */}
-        <StBookingButton>Забронировать</StBookingButton>
+        <StProductFooter>
+          <StProductFooterInfo>
+            <span>Стоимость за 1 ч.</span>
+            <StProductPrice>1600 ₽</StProductPrice>
+          </StProductFooterInfo>
+          <StProductFooterInfo>
+            <span>Стоимость за смену 8 ч.</span>
+            <StProductPrice>12800 ₽</StProductPrice>
+          </StProductFooterInfo>
+          <ButtonBooking />
+        </StProductFooter>
       </article>
     </StSection>
   );
