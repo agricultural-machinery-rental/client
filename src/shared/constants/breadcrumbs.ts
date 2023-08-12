60f74049dbd733cd66335058e810305efda2edcd
Dmitriy <63591058+advatunes@users.noreply.github.com>
2023-08-12 20:48:45 +0300

    #13252844 Хлебные крошки (#44)
    1. Task: верстка Хлебные крошки
    
    2. Screenshot:
    ![Screenshot 2023-08-10
    132759](https://github.com/agricultural-machinery-rental/client/assets/63591058/cbe8eefb-0c6f-4570-a3d4-da055e37a186)
    
    
    4. Was done:
       - [x] верстка Хлебные крошки (подстановка вручную через пропсы)
       - [x] подставление хлебных крошек из константы
    
    ---------
    
    Co-authored-by: advatunes <dj-9-1-1@yandex.ru>

---
+++ src/shared/constants/breadcrumbs.ts
@@ -0,0 +1,12 @@
+import { PATH } from '@/shared/constants/path';
+
+export const BREADCRUMBS = [
+  { text: 'Главная', url: PATH.MainPage },
+  { text: 'О компании', url: PATH.About },
+  { text: 'Каталог', url: PATH.Catalog },
+  { text: 'Профиль', url: PATH.Profile },
+  { text: 'Текущие заявки', url: PATH.Orders },
+  { text: 'История бронирований', url: PATH.BookingHistory },
+  { text: 'Избранное', url: PATH.Favourites },
+  { text: 'Настройки', url: PATH.Settings },
+];
