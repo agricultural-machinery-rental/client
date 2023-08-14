cf005131b2c72b2890eb97aac5b578bb192a2857
Elena Nikiforova <89295707+Lakatosska@users.noreply.github.com>
2023-08-14 11:40:53 +0200

    #13797621: Модалка редактирования и отмены бронирования (#63)
    1. Task: Модалка редактирования и отмены бронирования
    
    2. Screenshot:
    
    ![image](https://github.com/agricultural-machinery-rental/client/assets/89295707/2a999aaa-a990-4624-b80e-a4390245fd51)
    
    ![image](https://github.com/agricultural-machinery-rental/client/assets/89295707/d2efed2d-2d3a-4712-8232-9378c58d95c7)
    
    ![image](https://github.com/agricultural-machinery-rental/client/assets/89295707/ade9c0a2-8585-43be-bd2d-aec0c4416385)
    
    3. Was done:
       - [x] Модалка редактирования бронирования
       - [x] Модалка подтверждения отмены бронирования
       - [x] Модалка успешной отмены бронирования

--- src/widgets/user/Booking/index.ts
+++ src/widgets/user/Booking/index.ts
@@ -2,3 +2,6 @@
 
 export { BookingForm } from './ui';
 export { BookingSuccess } from './BookingSuccess/ui';
+export { BookingEditForm } from './BookingEdit/ui';
+export { BookingCancel } from './BookingCancel/ui';
+export { BookingCancelSuccess } from './BookingCancelSuccess/ui';
