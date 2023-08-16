77b4f617e322bd47dd6a9437b39eea0b817b0203
Nikolay Hoft <51874769+mrHoft@users.noreply.github.com>
2023-08-16 17:28:27 +0300

    #15817687 Фильтры. Сброс. (#67)

--- src/widgets/Catalog/filters/ui.tsx
+++ src/widgets/Catalog/filters/ui.tsx
@@ -1,8 +1,13 @@
 import { FiltersField } from './Field/ui';
 import { filters } from './filters';
+import { ResetField } from './Reset/ui';
 import { StWrapFilters } from './styled';
 
 export const Filters = () => {
+  const resetFilters = () => {
+    console.log('Reset filters');
+  };
+
   return (
     <StWrapFilters>
       <ul>
@@ -13,6 +18,9 @@ export const Filters = () => {
             </li>
           );
         })}
+        <li>
+          <ResetField callback={resetFilters} />
+        </li>
       </ul>
     </StWrapFilters>
   );
