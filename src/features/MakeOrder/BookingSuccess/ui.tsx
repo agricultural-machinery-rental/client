7077ecc86ed99651b60ac9d0e03dabfe00dc2273
Elena Nikiforova <vizualnoe@gmail.com>
2023-08-16 11:56:18 +0200

    fix Link on Button

--- src/features/MakeOrder/BookingSuccess/ui.tsx
+++ src/features/MakeOrder/BookingSuccess/ui.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link';
+<<<<<<< HEAD:src/features/MakeOrder/BookingSuccess/ui.tsx
 import { useRouter } from 'next/navigation';
 import { FC } from 'react';
+=======
+import { useRouter } from 'next/router';
+import { FC, useContext } from 'react';
+>>>>>>> d8a296b (fix Link on Button):src/widgets/user/Booking/BookingSuccess/ui.tsx
 
 import { useModalContext } from '@/entities/Modal';
 
@@ -10,12 +15,21 @@ import { StFlex, StTextBox } from '@/shared/styles/global';
 import { StContainer, StTitle, StWrapper, StSubtitle, StButton } from './styled';
 import type { TBookingSuccess } from './typing';
 
+<<<<<<< HEAD:src/features/MakeOrder/BookingSuccess/ui.tsx
 export const BookingSuccess: FC<TBookingSuccess> = ({ productName, daterange, message }) => {
   const router = useRouter();
   const { closeModal } = useModalContext();
 
   const handleClick = () => {
     router.push(PATH.MainPage);
+=======
+export const BookingSuccess: FC = () => {
+  const router = useRouter();
+  const { closeModal } = useContext(ModalContext);
+
+  const handleClick = () => {
+    router.push('/');
+>>>>>>> d8a296b (fix Link on Button):src/widgets/user/Booking/BookingSuccess/ui.tsx
     closeModal();
   };
 
@@ -42,14 +56,12 @@ export const BookingSuccess: FC<TBookingSuccess> = ({ productName, daterange, me
           <StTextBox $fontSize={0.67} $lineHeight={1} $textAlign='center'>
             Наш оператор скоро свяжется с Вами для уточнения деталей
           </StTextBox>
-          <Link href='/'>
-            <StButton
-              type='button'
-              label='На главную'
-              $designType='secondary'
-              onClick={handleClick}
-            />
-          </Link>
+          <StButton
+            type='button'
+            label='На главную'
+            $designType='secondary'
+            onClick={handleClick}
+          />
         </StFlex>
       </StWrapper>
     </StContainer>
