e509a1667228f87b59736ae4a5066d2591ddd6dc
mrHoft <ru.hoft@gmail.com>
2023-08-26 08:07:15 +0300

    feat: category links

--- src/widgets/CategoryItemList/api/data.tsx
+++ src/widgets/CategoryItemList/api/data.tsx
@@ -1,45 +1,55 @@
 import { TCategoryItemContainer } from '@/entities/Product/CategoryItemContainer';
 
-import foto_1 from './assets/foto-1.png';
-import foto_2 from './assets/foto-2.png';
-import foto_3 from './assets/foto-3.png';
-import foto_4 from './assets/foto-4.png';
-import foto_5 from './assets/foto-5.png';
-import foto_6 from './assets/foto-6.png';
-import foto_7 from './assets/foto-7.png';
-import foto_8 from './assets/foto-8.png';
+import { catalogItemCategory } from '@/shared/catalog';
+
+import photo_1 from './assets/foto-1.png';
+import photo_2 from './assets/foto-2.png';
+import photo_3 from './assets/foto-3.png';
+import photo_4 from './assets/foto-4.png';
+import photo_5 from './assets/foto-5.png';
+import photo_6 from './assets/foto-6.png';
+import photo_7 from './assets/foto-7.png';
+import photo_8 from './assets/foto-8.png';
 
 export const CategoryItemData: TCategoryItemContainer[] = [
   {
-    image: foto_1,
+    image: photo_1,
     title: 'Тракторы',
+    category: catalogItemCategory.tractor,
   },
   {
-    image: foto_2,
-    title: 'Комбаины',
+    image: photo_2,
+    title: 'Комбайны',
+    category: catalogItemCategory.harvester,
   },
   {
-    image: foto_3,
+    image: photo_3,
     title: 'Посевная техника',
+    category: catalogItemCategory.seeding,
   },
   {
-    image: foto_4,
+    image: photo_4,
     title: 'Мототракторы',
+    category: catalogItemCategory.motor_tractor,
   },
   {
-    image: foto_5,
+    image: photo_5,
     title: 'Прицепы',
+    category: catalogItemCategory.trailer,
   },
   {
-    image: foto_6,
+    image: photo_6,
     title: 'Плуги',
+    category: catalogItemCategory.plow,
   },
   {
-    image: foto_7,
+    image: photo_7,
     title: 'Погрузчики',
+    category: catalogItemCategory.loader,
   },
   {
-    image: foto_8,
+    image: photo_8,
     title: 'Навесное оборудование',
+    category: catalogItemCategory.attachment,
   },
 ];
