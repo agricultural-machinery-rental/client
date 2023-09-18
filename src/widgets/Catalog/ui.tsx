c340cdd38a06981f86e20a71750481f1ca3b9067
djpont <alexandervotyakov@gmail.com>
2023-09-18 04:40:13 +0400

    feat: url теперь выглядит так /название-категории/

--- src/widgets/Catalog/ui.tsx
+++ src/widgets/Catalog/ui.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-
 import { MakeOrder } from '@/features/MakeOrder';
 import { Signin } from '@/features/Signin';
 
@@ -7,30 +5,15 @@ import { Item } from '@/entities/Catalog';
 import { useModalContext } from '@/entities/Modal';
 
 import { getFilteredItemsData } from '@/shared/catalog';
-import { useCategoryByUrl } from '@/shared/hooks';
 import { useCatalogFilters } from '@/shared/model/filterContext';
 import { TMachineryDto } from '@/shared/model/typing';
-import { useAppDispatch, useAppSelector } from '@/shared/store';
-import {
-  isLoading as isMachineriesLoading,
-  getMachineries,
-  fetchGetMachineries,
-} from '@/shared/store/machineries';
 import { useGetUser } from '@/shared/store/user';
 
 import { StCatalogFlex } from './styled';
-
-export const Catalog = () => {
+export const Catalog = ({ machineries }: { machineries: TMachineryDto[] }) => {
   const user = useGetUser();
   const { openModal } = useModalContext();
   const { filters } = useCatalogFilters();
-
-  const category = useCategoryByUrl();
-
-  const dispatch = useAppDispatch();
-
-  const isLoading = useAppSelector(isMachineriesLoading);
-  const machineries = useAppSelector(getMachineries);
 
   const openModalWithContent = (data: TMachineryDto) => {
     if (user) {
@@ -44,14 +27,6 @@ export const Catalog = () => {
       openModal(<Signin />);
     }
   };
-
-  useEffect(() => {
-    dispatch(fetchGetMachineries(category));
-  }, [category, dispatch]);
-
-  if (isLoading) {
-    return <b>Загрузка...</b>;
-  }
 
   const filteredItemsData = getFilteredItemsData(filters, machineries);
 
