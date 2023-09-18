93377a4d803ecc9e002a4adde6220a80379c5472
djpont <alexandervotyakov@gmail.com>
2023-09-18 07:31:34 +0400

    feat: компонент теперь использует данные из стора

--- src/widgets/user/CurrentBookingsList/ui.tsx
+++ src/widgets/user/CurrentBookingsList/ui.tsx
@@ -1,20 +1,31 @@
+import { useEffect } from 'react';
+
 import { EditBooking } from '@/features/EditBooking';
 
 import { BookingCurrentItem } from '@/entities/BookingCurrentItem';
 import { useModalContext } from '@/entities/Modal';
 
 import Edit from '@/shared/icons/edit.svg';
+import { useAppDispatch, useAppSelector } from '@/shared/store';
+import { fetchGetOrders, getOrders } from '@/shared/store/orders';
 
-import { orders } from './constants';
 import { StButton, StContainer } from './styled';
 
 const CurrentBookingsList = () => {
   const { openModal } = useModalContext();
 
+  const dispatch = useAppDispatch();
+
+  const orders = useAppSelector(getOrders);
+
+  useEffect(() => {
+    dispatch(fetchGetOrders());
+  }, [dispatch]);
+
   return (
     <section>
       <StContainer $flexDirection='column' $gap={16}>
-        {orders.map((orderData, key) => {
+        {orders?.map((orderData, key) => {
           const EditButton = () => (
             <StButton
               onClick={() =>
@@ -28,7 +39,7 @@ const CurrentBookingsList = () => {
           );
 
           return (
-            <BookingCurrentItem key={key} {...orderData}>
+            <BookingCurrentItem key={key} orderData={orderData}>
               <EditButton />
             </BookingCurrentItem>
           );
