baaf044fd488f9f39207c2a37205975313f2b8cf
djpont <alexandervotyakov@gmail.com>
2023-09-18 08:18:28 +0400

    feat: фильтрация заказов от завершённых и отменённых

--- src/widgets/user/CurrentBookingsList/ui.tsx
+++ src/widgets/user/CurrentBookingsList/ui.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { EditBooking } from '@/features/EditBooking';
 
@@ -6,48 +6,61 @@ import { BookingCurrentItem } from '@/entities/BookingCurrentItem';
 import { useModalContext } from '@/entities/Modal';
 
 import Edit from '@/shared/icons/edit.svg';
+import { TOrderDto } from '@/shared/model/typing';
+import { orderStatus } from '@/shared/orders';
 import { useAppDispatch, useAppSelector } from '@/shared/store';
-import { fetchGetOrders, getOrders } from '@/shared/store/orders';
+import { fetchGetOrders, getOrders, isLoading } from '@/shared/store/orders';
 
 import { StButton, StContainer } from './styled';
 
 const CurrentBookingsList = () => {
   const { openModal } = useModalContext();
 
+  const [activeOrders, setActiveOrders] = useState<TOrderDto[]>();
+
   const dispatch = useAppDispatch();
 
   const orders = useAppSelector(getOrders);
+  const loading = useAppSelector(isLoading);
 
   useEffect(() => {
     dispatch(fetchGetOrders());
   }, [dispatch]);
 
+  useEffect(() => {
+    setActiveOrders(
+      orders?.filter(
+        order => order.status !== orderStatus.cancelled && order.status !== orderStatus.finished,
+      ),
+    );
+  }, [orders]);
+
+  if (!orders && loading) return <p>Загрузка...</p>;
+
+  if (!activeOrders || !activeOrders.length) return <p>Нет текущих заказов</p>;
+
   return (
     <section>
       <StContainer $flexDirection='column' $gap={16}>
-        {orders ? (
-          orders.map((orderData, key) => {
-            const EditButton = () => (
-              <StButton
-                onClick={() =>
-                  openModal(
-                    <EditBooking productId={''} productName={''} daterange={[]} message={''} />,
-                  )
-                }
-              >
-                <Edit width={24} height={24} />
-              </StButton>
-            );
+        {activeOrders.map((orderData, key) => {
+          const EditButton = () => (
+            <StButton
+              onClick={() =>
+                openModal(
+                  <EditBooking productId={''} productName={''} daterange={[]} message={''} />,
+                )
+              }
+            >
+              <Edit width={24} height={24} />
+            </StButton>
+          );
 
-            return (
-              <BookingCurrentItem key={key} orderData={orderData}>
-                <EditButton />
-              </BookingCurrentItem>
-            );
-          })
-        ) : (
-          <p>Нет заказов</p>
-        )}
+          return (
+            <BookingCurrentItem key={key} orderData={orderData}>
+              <EditButton />
+            </BookingCurrentItem>
+          );
+        })}
       </StContainer>
     </section>
   );
